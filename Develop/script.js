c9340a33a1457c243aabfb041ce0bf6505544651
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-09-29 18:26:05 -0400

    Declaration of Variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,47 @@
+//Declaration of Variables 
+
+
+const special_char=["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","'\'","]","^","_","`","{","|","}","~"];
+const numeric_char=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const alphabetUppercase_char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const alphabetLowercase_char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
