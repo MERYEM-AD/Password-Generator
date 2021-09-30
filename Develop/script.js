b7827a46713c8a645078c6cb9f192690a73c5809
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-09-29 23:49:00 -0400

    Write Special_ch () function block

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,11 @@ const numeric_char=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const alphabetUppercase_char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const alphabetLowercase_char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 let userNumber ;
+let randomList=[];
 const generateCode=1;
 
 /***** getNumber() function tests if the length of the password entered by the USER is at least 8 characters and no more than 128 characters 
- * IF YES the function will return the length number ;
+ * IF TRUE the function will return the length number ;
  * IF NOT the function will re-ask the USER to re-enter the right number which is at least 8 and no more than 128 ****** */
 
 function getNumber() { 
@@ -35,6 +36,21 @@ function getNumber() {
  
                         }
                       }
+
+
+
+/***** Special_ch(x) function tests if the the User confirm that the special charactere as part of password
+ * IF TRUE  then apply a random function on the special charactere array and add it into a randoList collection
+ * IF NOT Skip  */
+
+function Special_ch(optionChar) {
+
+          if(optionChar){
+
+                      index = Math.floor(Math.random() * special_char.length);
+                      randomList.unshift(special_char[index]) ;
+                    }
+                  }
 
 
 
