543630d897a6e1c6fa69ddafe78a3458b73c2f0b
MERYEM-AD <adiri.meryem.mw@gmail.com>
2021-09-30 01:06:42 -0400

    Write generationpassword(x) and freepassword(x) function block and invok function inside writePassword

--- Develop/script.js
+++ Develop/script.js
@@ -5,8 +5,10 @@ const special_char=["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",
 const numeric_char=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const alphabetUppercase_char = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const alphabetLowercase_char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-let userNumber ;
+let q2,q3,q4,q5; //question 1 inside getNumber() function
+let userNumber,index;
 let randomList=[];
+let password=[];
 const generateCode=1;
 
 
@@ -44,9 +46,9 @@ function getNumber() {
  * IF TRUE  then apply a random function on the special charactere array and add it into a randoList collection
  * IF NOT Skip  */
 
-function Special_ch(optionChar) {
+function Special_ch(s) {
 
-          if(optionChar){
+          if(s){
 
                       index = Math.floor(Math.random() * special_char.length);
                       randomList.unshift(special_char[index]) ;
@@ -59,9 +61,9 @@ function Special_ch(optionChar) {
  * IF NOT Skip  */
 
 
- function Numeric_ch(optionChar) {
+ function Numeric_ch(n) {
 
-          if(optionChar){
+          if(n){
 
                       index = Math.floor(Math.random() * numeric_char.length);
                       randomList.push(numeric_char[index]) ;
@@ -73,9 +75,9 @@ function Special_ch(optionChar) {
  * IF TRUE  then apply a random function on the alphabetUppercase charactere array and add it into a randoList collection
  * IF NOT Skip  */
 
- function Uppercase_ch(optionChar) {
+ function Uppercase_ch(u) {
 
-            if(optionChar){
+            if(u){
 
                         index = Math.floor(Math.random() *  alphabetUppercase_char.length);
                         randomList.push( alphabetUppercase_char[index]) ;
@@ -93,15 +95,15 @@ function Special_ch(optionChar) {
  
  *  3)- ELSE SKIP (don't do NOTHING)*/
 
- 
- function Lowercase_ch(optionChar) {
 
-            if((optionChar) && ((randomList.length)!== 0) ){   // T + T = T
+ function Lowercase_ch(l) {
+
+            if((l==true) && ((randomList.length)!== 0) ){   // T + T = T
               
                         index = Math.floor(Math.random() *  alphabetLowercase_char.length);
                         randomList.push(alphabetLowercase_char[index]);
               }
-            else if(((optionChar) && ((randomList.length)== 0)) || ((!optionChar) && ((randomList.length)== 0))  ){ //T+F=F OR F+F=F
+            else if(((l) && ((randomList.length)== 0)) || ((!l) && ((randomList.length)== 0))  ){ //T+F=F OR F+F=F
                         index = Math.floor(Math.random() *  alphabetLowercase_char.length);
                         randomList.unshift(alphabetLowercase_char[index]) ;
               }
@@ -117,36 +119,63 @@ function Special_ch(optionChar) {
 
 
 
+/***** generatePassword(x) function return the password as string after fill up it with the randomList collection */ 
+
+  function generatePassword(Number) {
+
+              for(let i=0;i<Number;i=password.length){
+                
+                for(let y=0;y<randomList.length;y++){
+                  
+                  password[i]=randomList[y];
+                  i++;
+                 }
+                 randomList=[];
+                Special_ch(q2);
+                Numeric_ch(q3);
+                Uppercase_ch(q4);
+                Lowercase_ch(q5);
+              }
+              
+              let code = password.splice((Number),(password.length-Number));
+              randomList=[];
+              return password.join("");
+             }
 
 
+/***** freePassword(x) function reset the password array after generation the password */          
+  function freePassword(p) {
+               password=[];
+              }
+  
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
 
 
+let Number_char =getNumber();
+q2= window.confirm("Do you want Special charactere : ");
+q3=window.confirm("Do you want numeric charactere : ");
+q4=window.confirm("Do you want UpperCase charactere : ");
+q5=window.confirm("Do you want Lowercase charactere : ");
 
 
+Special_ch(q2);
+Numeric_ch(q3);
+Uppercase_ch(q4);
+Lowercase_ch(q5);
 
 
+  var password = generatePassword(Number_char);
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+  freePassword(password);
 
+}
 
-
-
-
-
-
-
-
-
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
